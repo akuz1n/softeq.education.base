4a01097a5ce0ffb518910e340042bb01b9bfb736
Anton Kuzin <Anton.Kuzin@softeq.com>
2022-10-24 11:18:52 +0300

    UserRequest validators are added

---
+++ src/UsersManagement/TrialsSystem.UsersService/TrialsSystem.UsersService.Api/Application/Validation/CreateUserRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TrialsSystem.UsersService.Infrastructure.Models.UserDTOs;
+
+namespace TrialsSystem.UsersService.Api.Application.Validation
+{
+    public class CreateUserRequestValidator : AbstractValidator<CreateUserRequest>
+    {
+        public CreateUserRequestValidator()
+        {
+            RuleFor(x => x.BirthDate).LessThan(DateTime.Now.AddYears(-18));
+            RuleFor(x => x.Name).Must(value => !string.IsNullOrWhiteSpace(value));
+            RuleFor(x => x.Surname).Must(value => !string.IsNullOrWhiteSpace(value));
+            RuleFor(x => x).Must(value => !string.Equals(value.Surname, value.Name, StringComparison.InvariantCultureIgnoreCase));
+            RuleFor(x => x.Email).EmailAddress();
+        }
+    }
+}
--- src/UsersManagement/TrialsSystem.UsersService/TrialsSystem.UsersService.Api/Application/Validation/EditUserRequestValidator.cs
+++ src/UsersManagement/TrialsSystem.UsersService/TrialsSystem.UsersService.Api/Application/Validation/EditUserRequestValidator.cs
@@ -9,6 +9,12 @@ namespace TrialsSystem.UsersService.Api.Application.Validation
         {
             RuleFor(u => u.BirthDate)
                 .Must(u => u < DateTime.Now.AddYears(-18)).WithMessage("The participant should be older than 18 years.");
+            RuleFor(x => x.Name)
+                .Must(value => !string.IsNullOrWhiteSpace(value)).WithMessage("Name shouldn't be empty or whitespace");
+            RuleFor(x => x.Surname)
+                .Must(value => !string.IsNullOrWhiteSpace(value)).WithMessage("Surname shouldn't be empty or whitespace");
+            RuleFor(x => x)
+                .Must(value => !string.Equals(value.Surname, value.Name, StringComparison.InvariantCultureIgnoreCase)).WithMessage("Name and surname should be different");
         }
     }
 }
