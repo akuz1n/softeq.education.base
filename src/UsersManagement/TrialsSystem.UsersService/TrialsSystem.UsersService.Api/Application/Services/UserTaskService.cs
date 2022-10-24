138fe0e62ce6b30312146caee64b312b09d32504
Anton Kuzin <Anton.Kuzin@softeq.com>
2022-10-25 02:14:48 +0300

    UserTask entity created. Basic UserTaskService is created

---
+++ src/UsersManagement/TrialsSystem.UsersService/TrialsSystem.UsersService.Api/Application/Services/UserTaskService.cs
@@ -0,0 +1,12 @@
+﻿using TrialsSystem.UsersService.Domain.AggregatesModel;
+
+namespace TrialsSystem.UsersService.Api.Application.Services
+{
+    public class UserTaskService
+    {
+        public async Task<IEnumerable<UserTask>> GetAll()
+        {
+            return await Task.FromResult(new List<UserTask>());
+        }
+    }
+}
---
+++ src/UsersManagement/TrialsSystem.UsersService/TrialsSystem.UsersService.Domain/AggregatesModel/UserTask.cs
@@ -0,0 +1,13 @@
+﻿using TrialsSystem.UsersService.Domain.AggregatesModel.Base;
+
+namespace TrialsSystem.UsersService.Domain.AggregatesModel
+{
+    public class UserTask : Entity
+    {
+        public string Name { get; set; }
+
+        public string Status { get; set; }
+
+        public Dictionary<string, string> AdditionalProperties { get; set; }
+    }
+}
