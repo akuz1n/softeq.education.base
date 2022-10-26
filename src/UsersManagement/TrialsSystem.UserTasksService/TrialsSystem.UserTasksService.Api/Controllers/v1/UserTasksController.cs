7fe8d7e56784f15a8f7063926c28f059e4f14f7f
Anton Kuzin <Anton.Kuzin@softeq.com>
2022-10-26 10:25:21 +0300

    Base sceleton for user tasks microservice

---
+++ src/UsersManagement/TrialsSystem.UserTasksService/TrialsSystem.UserTasksService.Api/Controllers/v1/UserTasksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrialsSystem.UserTasksService.Api.Controllers.v1
+{
+    [Route("api/v1{userId}/controller]")]
+    [ApiController]
+    public class UserTasksController : ControllerBase
+    {
+    }
+}
---
+++ src/UsersManagement/TrialsSystem.UserTasksService/TrialsSystem.UserTasksService.Domain/UserTask.cs
@@ -0,0 +1,11 @@
+﻿namespace TrialsSystem.UserTasksService.Domain
+{
+    public class UserTask : Entity
+    {
+        public string Name { get; set; }
+
+        public string Status { get; set; }
+
+        public Dictionary<string, string> AdditionalProperties { get; set; }
+    }
+}
