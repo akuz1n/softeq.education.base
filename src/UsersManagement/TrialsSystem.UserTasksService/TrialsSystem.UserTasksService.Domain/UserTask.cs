31999e5d0451ec968258a875069d6fa048a6f01e
Anton Kuzin <Anton.Kuzin@softeq.com>
2022-10-27 14:19:41 +0300

    UserTasks entity objects are made immutable

--- src/UsersManagement/TrialsSystem.UserTasksService/TrialsSystem.UserTasksService.Domain/UserTask.cs
+++ src/UsersManagement/TrialsSystem.UserTasksService/TrialsSystem.UserTasksService.Domain/UserTask.cs
@@ -2,10 +2,10 @@
 {
     public class UserTask : Entity
     {
-        public string Name { get; set; }
+        public string Name { get; private set; }
 
-        public string Status { get; set; }
+        public string Status { get; private set; }
 
-        public Dictionary<string, string> AdditionalProperties { get; set; }
+        public Dictionary<string, string> AdditionalProperties { get; private set; }
     }
 }
